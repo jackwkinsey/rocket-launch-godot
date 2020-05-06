676af475ba702f7a356216e5f122a5609687dc38
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-06 00:16:36 -0400

    feat: set rocket ship sprite anim to launch

--- World.gd
+++ World.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 onready var animation_player = $AnimationPlayer
+onready var rocket_ship_sprite = $RocketShip/Sprite
 
 func _ready():
 	pass
 
-
 func _on_LaunchButton_pressed():
 	animation_player.play("Launch")
+	rocket_ship_sprite.animation = "Launch"
